0ac3f8bff934ac71ed48b19cea93f4f4b3264b68
alanbiju78 <alanbiju78@gmail.com>
2024-06-29 15:27:45 +0530

    Friendsapp

--- app/src/main/java/com/example/friendsapp/MainActivity.java
+++ app/src/main/java/com/example/friendsapp/MainActivity.java
@@ -1,20 +1,78 @@
 package com.example.friendsapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    EditText e1,e2,e3,e4;
+    AppCompatButton b1;
+    String apiurl="https://friendsapi-re5a.onrender.com/adddata";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        e1=findViewById(R.id.name);
+        e2=findViewById(R.id.fname);
+        e3=findViewById(R.id.fnname);
+        e4=findViewById(R.id.fdesc);
+        b1=findViewById(R.id.subbt);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String getName=e1.getText().toString();
+                String getFname=e2.getText().toString();
+                String getFnname=e3.getText().toString();
+                String getFdesc=e4.getText().toString();
+                //json object creation
+                JSONObject friend=new JSONObject();
+                try {
+                    friend.put("name",getName);
+                    friend.put("friendName",getFname);
+                    friend.put("friendNickName",getFnname);
+                    friend.put("DescribeYourFriend",getFdesc);
+                   ;
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+//jsonobject request creation
+                JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(
+                        Request.Method.POST, apiurl, friend,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(getApplicationContext(), "Added successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Failed to add", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+                requestQueue.add(jsonObjectRequest);
 
+
+            }
+        });
     }
 }
